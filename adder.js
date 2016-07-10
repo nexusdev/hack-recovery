61f4599bccc0d8292fe880ccdac085b84bfdf618
Denis Erfurt <denis.erfurt@gmail.com>
2016-07-10 14:31:00 +0200

    remove maker-otc balance

--- adder.js
+++ adder.js
@@ -10,8 +10,11 @@ var addresses = Object.keys(dsEthToken).concat(Object.keys(simpleMarket));
 addresses = _.uniq(addresses);
 
 var restored = {};
+var total = new BigNumber(0);
 
 addresses.forEach(address => {
+  // skip maker otc address
+  if(address === "0xf51bc4633f5924465c8c6317169faf3e4312e82f") return;
   restored[address] = new BigNumber(0);
 
   if(address in dsEthToken) {
@@ -21,6 +24,7 @@ addresses.forEach(address => {
   if(address in simpleMarket && "ETH" in simpleMarket[address]) {
     restored[address] = restored[address].add(new BigNumber(simpleMarket[address].ETH));
   }
+  total = total.add(restored[address]);
 });
 
 // remove zero balances
@@ -29,3 +33,4 @@ restored = _.omitBy(restored, bal => bal.toString() === '0');
 var formated = _.map(restored, (bal, addr) => `${addr}: ${bal.toString(10)}`).join('\n');
 fs.writeFileSync("balances.txt", formated, {encoding: "utf8"});
 console.log(formated);
+console.log(total.toString(10));
